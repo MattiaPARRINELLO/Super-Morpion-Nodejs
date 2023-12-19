233e3a4aeff32d29e323dfe36ea947bb04ea042e
Mattia PARRINELLO <contactperso.mp@gmail.com>
2023-12-19 18:17:02 +0100

    Add user count and limit connection to 2 users

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require("express");
+const { connect } = require("http2");
 const app = express();
 const port = 3000;
 var http = require("http").Server(app);
 const io = require("socket.io")(http);
-const connectedUsers = { "Player 1": false, "Player 2": false };
-
+let nbUser = 0;
 app.get("/1", (req, res) => res.sendFile(__dirname + "/view/Joueur 1/"));
 app.get("/2", (req, res) => res.sendFile(__dirname + "/view/Joueur 2/"));
 
 io.on("connection", function (socket) {
+  nbUser++;
+  console.log(nbUser);
+  if (nbUser > 2) {
+    console.log("Too many users");
+    socket.broadcast.emit("state", "Too many users");
+    socket.disconnect();
+    nbUser--;
+  }
   socket.on("casePlayedJ2", (caseID) => {
     console.log("casePlayedJ2", caseID);
     let bigCase = caseID[0];
@@ -25,6 +33,10 @@ io.on("connection", function (socket) {
     let smallCase = caseID[3];
     let list = [bigCase, smallCase];
     socket.broadcast.emit("casePlayedServerJ1", list);
+  });
+  socket.on("disconnect", function () {
+    console.log("user disconnected");
+    nbUser--;
   });
 });
 // casePlayedServerJ1
